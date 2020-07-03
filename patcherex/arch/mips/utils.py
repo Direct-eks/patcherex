e8094b6efb78253817159c5b6879982fbdedbdce
Direct-eks <35151167+Direct-eks@users.noreply.github.com>
2020-07-02 22:36:38 -0400

    mips

--- patcherex/arch/mips/utils.py
+++ patcherex/arch/mips/utils.py
@@ -6,22 +6,28 @@ def capstone_to_asm(instruction):
     return instruction.mnemonic + " " + instruction.op_str.replace('{', '{{').replace('}', '}}')
 
 
-def disassemble(code, offset=0x0, is_64=False):
-    md = capstone.Cs(capstone.CS_ARCH_MIPS, capstone.CS_MODE_MIPS64 if is_64 else capstone.CS_MODE_MIPS32)
+def disassemble(code, offset=0x0, bits=32):
+    if bits == 32:
+        md = capstone.Cs(capstone.CS_ARCH_MIPS, capstone.CS_MODE_MIPS32)
+    elif bits == 64:
+        md = capstone.Cs(capstone.CS_ARCH_MIPS, capstone.CS_MODE_MIPS64)
+    else:
+        raise Exception("disassemble(): Unsupported bits %d." % bits)
+
     md.detail = True
     if isinstance(code, str):
         code = bytes(map(ord, code))
     return list(md.disasm(code, offset))
 
 
-def compile_jmp(origin, target, is_thumb=False):
+def compile_jmp(origin, target, bits=32):
     jmp_str = '''
-        bl {target}
+        j {target}
     '''.format(**{'target': hex(int(target))})
-    return compile_asm(jmp_str, base=origin, is_thumb=is_thumb)
+    return compile_asm(jmp_str, base=origin, bits=bits)
 
 
-def compile_asm(code, base=None, name_map=None, is_thumb=False):
+def compile_asm(code, base=None, name_map=None, bits=32):
     # print "=" * 10
     # print code
     # if base != None: print hex(base)
@@ -34,7 +40,7 @@ def compile_asm(code, base=None, name_map=None, is_thumb=False):
     except KeyError as e:
         raise UndefinedSymbolException(str(e))
     try:
-        ks = keystone.Ks(keystone.KS_ARCH_ARM, keystone.KS_MODE_THUMB if is_thumb else keystone.KS_MODE_ARM)
+        ks = keystone.Ks(keystone.KS_ARCH_MIPS, keystone.KS_MODE_BIG_ENDIAN)
         encoding, count = ks.asm(code, base)
     except keystone.KsError as e:
         print("ERROR: %s" % e)  # TODO raise some error
@@ -56,7 +62,7 @@ def get_nasm_c_wrapper_code(function_symbol, get_return=False, debug=False):
     '''
     # if debug:
     #     wcode.append("int 0x3")
-    wcode.append("bl {%s}" % function_symbol)
+    wcode.append("j {%s}" % function_symbol)
     # if get_return:
     #     wcode.append("mov [esp+28], eax") #FIXME check
     # wcode.append("popa")
@@ -64,36 +70,36 @@ def get_nasm_c_wrapper_code(function_symbol, get_return=False, debug=False):
     return "\n".join(wcode)
 
 
-def compile_c(code, optimization='-Oz', name_map=None, compiler_flags="-m32", is_thumb=False):
+def compile_c(code, optimization='-Oz', name_map=None, compiler_flags="-m32"):
     # TODO symbol support in c code
     with tempdir() as td:
-        c_fname = os.path.join(td, "code.c")
-        object_fname = os.path.join(td, "code.o")
-        bin_fname = os.path.join(td, "code.bin")
+        code_filename = os.path.join(td, "code.c")
+        object_filename = os.path.join(td, "code.o")
+        bin_filename = os.path.join(td, "code.bin")
 
-        fp = open(c_fname, 'w')
+        fp = open(code_filename, 'w')
         fp.write(code)
         fp.close()
 
-        res = exec_cmd("clang -nostdlib -mno-sse -target arm-linux-gnueabihf -ffreestanding %s %s -o %s -c %s %s" \
-                       % ("-mthumb" if is_thumb else "", optimization, object_fname, c_fname, compiler_flags),
-                       shell=True)
+        res = exec_cmd("clang -nostdlib -target mips-linux-gnu -ffreestanding %s -o %s -c %s %s"
+                       % (optimization, object_filename, code_filename, compiler_flags), shell=True)
         if res[2] != 0:
             print("CLang error:")
             print(res[0])
             print(res[1])
-            fp = open(c_fname, 'r')
+            fp = open(code_filename, 'r')
             fcontent = fp.read()
             fp.close()
             print("\n".join(["%02d\t%s" % (i + 1, l) for i, l in enumerate(fcontent.split("\n"))]))
             raise CLangException
-        res = exec_cmd("arm-linux-gnueabihf-objcopy -O binary -j .text %s %s" % (object_fname, bin_fname), shell=True)
+        res = exec_cmd("mips-linux-gnu-objcopy -O binary -j .text %s %s" % (object_filename, bin_filename),
+                       shell=True)
         if res[2] != 0:
             print("objcopy error:")
             print(res[0])
             print(res[1])
             raise ObjcopyException
-        fp = open(bin_fname, "rb")
+        fp = open(bin_filename, "rb")
         compiled = fp.read()
         fp.close()
     return compiled
