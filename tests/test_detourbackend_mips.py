020a94fbc1c3ab38280b42c8d96754e0e3940a86
Direct-eks <35151167+Direct-eks@users.noreply.github.com>
2020-07-09 22:55:27 -0400

    tests

--- tests/test_detourbackend_mips.py
+++ tests/test_detourbackend_mips.py
@@ -22,26 +22,39 @@ class Tests(unittest.TestCase):
             os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries/tests/mips/patcherex'))
         self.qemu_location = shellphish_qemu.qemu_path('mips')
 
+    # test_printf.s is compiled from test_printf.c
+
+    # test1
+    # test_printf_test1.s is modified from test_printf.s
     def test_inline_patch(self):
         self.run_test("test", [InlinePatch(0x400750, "addiu $a1, $v0, 0x938")],
                       expected_output=b"%s", expected_returnCode=0)
 
+    # test2
+    # test_printf_test2.s is modified from test_printf.s
     def test_remove_instruction_patch(self):
         self.run_test("test", [RemoveInstructionPatch(0x400754, 4)],
                       expected_output=b"Hello", expected_returnCode=0)
 
     def test_add_code_patch(self):
         added_code = '''
-
+            # todo
         '''
         self.run_test("test", [AddCodePatch(added_code, "added_code")],
                       set_oep="added_code", expected_returnCode=0x32)
 
+    # test3
+    # test_printf_test3.s is modified from test_printf.s
     def test_insert_code_patch(self):
         test_str = b"abcdefghij\n\x00"
         added_code = '''
-            la $a0, added_data
-            jal printf
+            lw  $28,16($fp)
+            move    $2,$0
+            lui $2,%hi(added_data)
+            addiu   $4,$2,%lo(added_data)
+            lw	$2,%call16(printf)($28)
+            move    $25,$2
+            jalr    $25
         '''
         p1 = InsertCodePatch(0x400768, added_code)
         p2 = AddRODataPatch(test_str, "added_data")
@@ -49,50 +62,74 @@ class Tests(unittest.TestCase):
         self.run_test("test", [p1, p2], expected_output=b"Helloabcdefghij\n",
                       expected_returnCode=0)
 
+    # test4
+    # test_printf_test4.s is modified from test_printf.s
     def test_add_label_patch(self):
         p1 = AddLabelPatch(0x400934, "added_label")
         added_code = '''
-            la $a0, added_label
-            jal printf
+            lw	$28,16($fp)
+            move	$2,$0
+            lui	$2,%hi(added_label)
+            addiu	$4,$2,%lo(added_label)
+            lw	$2,%call16(printf)($28)
+            move	$25,$2
+            jalr	$25
         '''
         p2 = InsertCodePatch(0x400768, added_code)
 
         self.run_test("test", [p1, p2], expected_output=b"Helloo", expected_returnCode=0)
 
-    # def test_raw_file_patch(self):
-    #     self.run_test("printf_nopie", [RawFilePatch(0x44c, b"No")], expected_output=b"No",
-    #                   expected_returnCode=0)
+    def test_raw_file_patch(self):
+        self.run_test("printf_nopie", [RawFilePatch(0x00, b"No")], expected_output=b"No",
+                      expected_returnCode=0)
 
-    # def test_raw_mem_patch(self):
-    #     self.run_test("printf_nopie", [RawMemPatch(0x1044c, b"No")], expected_output=b"No",
-    #                   expected_returnCode=0)
+    def test_raw_mem_patch(self):
+        self.run_test("printf_nopie", [RawMemPatch(0x00, b"No")], expected_output=b"No",
+                      expected_returnCode=0)
 
+    # test5
+    # test_printf_test5.s is modified from test_printf.s
     def test_add_ro_data_patch(self, tlen=5):
         p1 = AddRODataPatch(b"A" * tlen, "added_data")
         added_code = '''
-            la $a0, added_data
-            jal printf
+            lw	$28,16($fp)
+            move	$2,$0
+            lui	$2,%hi(added_data)
+            addiu	$4,$2,%lo(added_data)
+            lw	$2,%call16(printf)($28)
+            move	$25,$2
+            jalr	$25
         ''' % tlen
         p2 = InsertCodePatch(0x400768, added_code, 'added_code')
 
         self.run_test("test", [p1, p2], expected_output=b"Hello" + b"A" * tlen,
                       expected_returnCode=0x0)
 
+    # test6 todo still in error
+    # test_printf_test6.s is modified from test_printf.s
     def test_add_rw_data_patch(self, tlen=5):
         p1 = AddRWDataPatch(tlen, "added_data_rw")
         added_code = '''
+            lw	$28,16($fp)
+            move	$2,$0
+            
             li $t0, 0x41
-            and $t1, $zero
-            li $t2, %d
-            la $v0, added_data_rw
+            li $t1, 0x0
+            li $t2, 0x5
+            lui	$v0,%hi(added_data_rw)
+            addiu	$v1,$v0,%lo(added_data_rw)
             loop:
                 beq $t1, $t2, exit
-                sw $t0, ($v0)
-                addiu $v0, $v0, 4
+                sb $t0, 0($v1) # something is wrong here
+                addiu $v1, $v1, 1
+                addiu $t1, $t1, 1
                 j loop
             exit:
-                la $a0, added_data_rw
-                jal printf
+                lui	$2,%hi(added_data_rw)
+                addiu	$4,$2,%lo(added_data_rw)
+                lw	$2,%call16(printf)($28)
+                move	$25,$2
+                jalr	$25
         ''' % tlen
         p2 = InsertCodePatch(0x400768, added_code, "modify_and_print")
 
@@ -102,8 +139,7 @@ class Tests(unittest.TestCase):
     def test_add_rw_init_data_patch(self, tlen=5):
         p1 = AddRWInitDataPatch(b"A" * tlen, "added_data_rw")
         added_code = '''
-            la $a0, added_data_rw
-            jal printf
+            
         ''' % tlen
         p2 = InsertCodePatch(0x400768, added_code, "print")
 
@@ -123,8 +159,8 @@ class Tests(unittest.TestCase):
         ''' % patcherex.arch.mips.utils.get_nasm_c_wrapper_code("c_function", get_return=True)
 
         self.run_test("test", [InsertCodePatch(0x103ec, added_code, name="p1", priority=1),
-                                       AddCodePatch("__attribute__((fastcall)) int func(int a){ return a + 1; }",
-                                                    "c_function", is_c=True, compiler_flags="")],
+                               AddCodePatch("__attribute__((fastcall)) int func(int a){ return a + 1; }",
+                                            "c_function", is_c=True, compiler_flags="")],
                       expected_output=b"sHi", expected_returnCode=0x0)
 
     def test_add_data_patch_long(self):
@@ -137,26 +173,13 @@ class Tests(unittest.TestCase):
     def test_complex1(self):
         patches = []
         added_code = '''
-                mov r7, 0x4
-                mov r0, 0x1
-                ldr r1, =0x10450
-                mov r2, 2
-                svc 0
-                bl {added_function}
-                mov r7, 0x1
-                mov r0, 0x34
-                svc 0
+                
             '''
         patches.append(AddEntryPointPatch(added_code))
 
         test_str = b"testtesttest\n\x00"
         added_code = '''
-                mov r7, 0x4
-                mov r0, 0x1
-                ldr r1, ={added_data}
-                mov r2, %d
-                svc 0
-                bx lr
+                
             ''' % (len(test_str))
         patches.append(AddCodePatch(added_code, "added_function"))
         patches.append(AddRODataPatch(test_str, "added_data"))
@@ -168,18 +191,10 @@ class Tests(unittest.TestCase):
         test_str1 = b"1111111111\n\x00"
         test_str2 = b"2222222222\n\x00"
         added_code1 = '''
-            mov r7, 0x4
-            mov r0, 0x1
-            ldr r1, ={str1}
-            mov r2, %d
-            svc 0
+            
         ''' % (len(test_str1))
         added_code2 = '''
-            mov r7, 0x4
-            mov r0, 0x1
-            ldr r1, ={str2}
-            mov r2, %d
-            svc 0
+            
         ''' % (len(test_str2))
 
         p1 = InsertCodePatch(0x103ec, added_code1, name="p1", priority=100)
